20f8d6a910816c384f3dd3ab442d4648926c33f9
nexxluo <670435363@qq.com>
2022-03-16 09:10:15 +0800

    chore : 完全导出@formily/react，避免外部使用useFieldSchema返回空

--- packages/formx-renderer/src/index.js
+++ packages/formx-renderer/src/index.js
@@ -1,7 +1,7 @@
 import FormRender, { FormContext } from "./render/index";
 import * as registry from "./core/registry";
-import { Schema } from "@formily/react";
 import { FormPath } from "@formily/shared";
 
-export { FormRender, FormPath, Schema, registry, FormContext };
+export { FormRender, FormPath, registry, FormContext };
+export * from "@formily/react";
 export default FormRender;
