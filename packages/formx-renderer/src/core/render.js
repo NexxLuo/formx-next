553e0541204f67ff8b3d0f945879c35139fb412f
nexxluo <670435363@qq.com>
2022-05-24 17:51:39 +0800

    bug fixed : maximum验证器未强转number导致未进行验证

--- packages/formx-renderer/src/core/render.js
+++ packages/formx-renderer/src/core/render.js
@@ -14,6 +14,7 @@ import {
     onFieldValidateEnd
 } from "@formily/core";
 import { getRegistryComponents } from "./getRegistryComponents";
+import "./validator/rule"
 
 let eachSchema = (schema, fn) => {
     if (schema) {
---
+++ packages/formx-renderer/src/core/validator/rule.ts
@@ -0,0 +1,33 @@
+import {
+    isEmpty,
+    isStr,
+    isValid,
+    isNum,
+    isArr,
+    stringLength
+} from "@formily/shared";
+import { registerValidateRules } from "@formily/core";
+
+const isValidateEmpty = (value: any) => {
+    if (isArr(value)) {
+        for (let i = 0; i < value.length; i++) {
+            if (isValid(value[i])) return false;
+        }
+        return true;
+    } else {
+        return isEmpty(value);
+    }
+};
+
+const getLength = (value: any) => {
+    return isStr(value) ? stringLength(value) : value ? value.length : 0;
+};
+
+registerValidateRules({
+    maximum(value, rule) {
+        if (isValidateEmpty(value)) return "";
+        var length = isNum(Number(value)) ? Number(value) : getLength(value);
+        var max = Number(rule.maximum);
+        return length > max ? rule.message : "";
+    }
+});
