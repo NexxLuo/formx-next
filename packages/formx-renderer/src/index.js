655c1a01e000f251174b79b98af63c7ecdb524b2
nexxluo <luoy@sysdsoft.cn>
2023-12-15 16:40:10 +0800

    feature：导出Decimal对象

--- packages/formx-renderer/src/index.js
+++ packages/formx-renderer/src/index.js
@@ -1,7 +1,8 @@
 import FormRender, { FormContext } from "./render/index";
 import * as registry from "./core/registry";
 import { FormPath } from "@formily/shared";
+import Decimal from "decimal.js";
 
-export { FormRender, FormPath, registry, FormContext };
+export { FormRender, FormPath, registry, FormContext, Decimal };
 export * from "@formily/react";
 export default FormRender;
