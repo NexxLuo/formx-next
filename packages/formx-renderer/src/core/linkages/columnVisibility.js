df8128c4163fdd5d7322bfc8f3dd3cdada28f6d1
nexxluo <luoy@sysdsoft.cn>
2024-06-27 14:48:28 +0800

    fixed : 表格列条件隐藏，当条件为主表字段联动时，可能表格还未挂载，此时无法表格列无法被正确隐藏

--- packages/formx-renderer/src/core/linkages/columnVisibility.js
+++ packages/formx-renderer/src/core/linkages/columnVisibility.js
@@ -6,7 +6,8 @@ export function linkageColumnVisibility(linkageItem, instance, _evaluator) {
             let itemKey = d.name;
             if (listPath) {
                 let list = instance.getFieldState(listPath);
-                if (list && list.fieldActions) {
+                //fixed : 如果表格还未挂载，执行此操作会导致初始化时无法正确隐藏表格列
+                if (list && list.fieldActions && list.mounted === true) {
                     let fn = list.fieldActions.toggleColumnVisibility;
                     if (typeof fn === "function") {
                         let res = _evaluator.evaluate(d.expression, {});
