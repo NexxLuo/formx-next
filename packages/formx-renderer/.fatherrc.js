e1ee2fa41238d7f9f12bea8fd833109b0f14d631
nexxluo <luoy@sysdsoft.cn>
2024-08-08 09:36:34 +0800

    chore : 更新father-build

--- packages/formx-renderer/.fatherrc.js
+++ packages/formx-renderer/.fatherrc.js
@@ -1,9 +1,6 @@
 export default {
-    entry: "src/index.js",
-    esm: false,
-    cssModules: false,
-    disableTypeCheck: true,
     cjs: {
-        type: "babel"
+      output: 'lib'
     }
-};
+  };
+  
